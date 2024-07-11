f4c844c2f559a87077c5ac09f7ad734b701d4bdd
Ruben Tura <rubentura@tuta.io>
2024-07-11 19:44:30 +0200

    added language persistence

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,8 +1,17 @@
 window.onload = function () {
     let lang = navigator.language.slice(0, 2);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const queryLang = urlParams.get('lang');
     //console.log(lang);
 
-    if (lang === "it") {
+    if (queryLang !== "") {
+        if (queryLang === "it") {
+            document.querySelectorAll('.lang-en').forEach(elem => elem.classList.add("lang-hidden"));
+        } else {
+            document.querySelectorAll('.lang-it').forEach(elem => elem.classList.add("lang-hidden"));
+        }
+    } else if (lang === "it") {
         document.querySelectorAll('.lang-en').forEach(elem => elem.classList.add("lang-hidden"));
     } else {
         document.querySelectorAll('.lang-it').forEach(elem => elem.classList.add("lang-hidden"));
